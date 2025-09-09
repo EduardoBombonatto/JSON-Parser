747f2c930d84db16caabbb0dc533d937a72394f4
EduardoBombonatto <eduardobombonattolorenzetti@gmail.com>
2025-09-09 17:41:29 -0300

    Step One: Verified if the JSON is '{}'

--- JSONParser.java
+++ JSONParser.java
@@ -1,27 +1,34 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class JSONParser {
-    public static void main(String[] args) {
-        if(args.length != 1){
+    public static void main(String[] var0) {
+        if (var0.length != 1) {
             System.err.println("Usage: java JSONParser <filename>");
             System.exit(1);
         }
-        String filename = args[0];
+
+        String fileName = var0[0];
 
         try {
-            File file = new File(filename);
-            Scanner reader = new Scanner(file);
+            String file = Files.readString(Path.of(fileName)).trim();
 
-            while(reader.hasNextLine()){
-                String data = reader.nextLine();
-                if(data.equals("{}")) System.out.println(0);
-                else System.out.println(1);
+            if (isValidJson(file)) {
+                System.out.println("Valid Json");
+                System.exit(0);
+            } else {
+                System.out.println("Invalid Json");
+                System.exit(1);
             }
-        } catch (FileNotFoundException e){
-            System.err.println("File not found: " + filename);
+
+        } catch (IOException e) {
+            System.err.println("Error reading file " + fileName);
             System.exit(1);
         }
+    }
+
+    private static boolean isValidJson(String json) {
+        return json.equals("{}");
     }
 }
